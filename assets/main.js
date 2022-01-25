8062fd04b14f35f72c9554f696c198653d75e4cc
nemesisco <97507569+nemesisco@users.noreply.github.com>
2022-01-26 03:04:47 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -62,14 +62,14 @@ if(localData == "light"){
     document.body.classList.remove("dark-mode");
 }
 else if(localData == "dark"){
-    darkModeBtn.src = "sun.png";
+    darkModeBtn.src = "assets/sun.png";
     document.body.classList.add("dark-mode");
 }
 
 darkModeBtn.onclick = function (){
     document.body.classList.toggle('dark-mode');
     if(document.body.classList.contains('dark-mode')){
-        darkModeBtn.src = "sun.png"
+        darkModeBtn.src = "assets/sun.png"
         localStorage.setItem("theme", "dark");
     }
     else{
